b184d1f120656d5fa1fcb35eb49fb5fdaf204e6e
brecklend <brecklend@gmail.com>
2016-09-21 16:42:41 -0700

    Transitioning code to syncroRojo

--- server.js
+++ server.js
@@ -4,40 +4,68 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-//check if /scrape needs to be the name of the directory: /scotch
-app.get('/scrape', function(req, res) {
-	var url = 'http://www.imdb.com/title/tt1229340/';
+app.get('/syncrorojo', function(req, res) {
+	//get array of city links within search states
+	//get listings for each city
+	//filter listings
+	//write to json file
+
+	//For development, get cl pages and host them locally
+	//var url = 'http://localhost/cl/sites';
+
+	var url = 'http://localhost:8081/sites.html';
 
 	request(url, function(error, response, html) {
 		if(!error) {
 			var $ = cheerio.load(html);
-			var title, release, rating;
-			var json = { title: "", release: "", rating: ""};
 
-			$('h1').filter(function() {
-				var data = $(this);
+			$('h1 a[name=US]').filter(function() {
+				var data = $(this).parent();
+				var divcolmask = data.siblings('.colmask').first();
+				var divbox = divcolmask.children('.box');
+				var h4ca = divbox.children('h4');
 
-				title = data.text();
-				release = data.children().first().children().first().text();
+				$(divbox).each(function(i, ele) {
+					console.log('State: ' + $(this).text());
+				});
+				// var container = data.find('h4').text() == 'Alaska';
+				// var cities = container.children();
 
-				json.title = title;
-				console.log('json.title: ' + json.title);
-				json.release = release;
+				// for(var i = 0; i < cities.length; i++) {
+				// 	console.log('city');
+				// }
 			});
 
-			$('span[itemprop=ratingValue]').filter(function() {
-				var data = $(this);
+			// var title, release, rating;
+			// var json = { title: "", release: "", rating: ""};
 
-				rating = data.text();
+			// $('h1').filter(function() {
+			// 	var data = $(this);
 
-				json.rating = rating;
-			});
+			// 	title = data.text();
+			// 	release = data.children().first().children().first().text();
 
-			fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
-				console.log('File successfully written! - Check your project directory for the output.json file');
-			});
+			// 	json.title = title;
+			// 	console.log('json.title: ' + json.title);
+			// 	json.release = release;
+			// });
 
-			res.send('Check your console!');
+			// $('span[itemprop=ratingValue]').filter(function() {
+			// 	var data = $(this);
+
+			// 	rating = data.text();
+
+			// 	json.rating = rating;
+			// });
+
+			// fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
+			// 	console.log('File successfully written! - Check your project directory for the output.json file');
+			// });
+
+			// res.send('Check your console!');
+		}
+		else {
+			console.log('error');
 		}
 	});
 });
