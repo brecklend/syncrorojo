87ce8cd96a0ab0f31e3718d671b4896cd42fa6c1
brecklend <brecklend@gmail.com>
2016-09-30 11:35:40 -0700

    Working on json pushing for city listings

--- server.js
+++ server.js
@@ -21,27 +21,30 @@ var cs = require("./_cl");
 			var $ = cheerio.load(html);
 			//var locationUrls = [];
 			var json = [];
+			var cnt = 0;
 
 			$("h4").filter(function() {
 				var stateName = $(this).text();
 				var searchStr = "Washington|Oregon|California|Arizona|Utah|Idaho|Nevada";
-				/*<<<DEV>>>*/ searchStr = "Arizona";
+				/*<<<DEV>>>*/ searchStr = "Arizona|California";
 				var searchStates = new RegExp(searchStr);
 
 				if (searchStates.exec(stateName)) {
-					//console.log('stateName:', stateName);
 					json.push({"State": { "Name": stateName, "Cities": []}});
 					
 					var cities = $(this).next('ul').children();
-					/*<<<DEV>>>*/ cities = '<li><a href="//flagstaff.craigslist.org/">flagstaff / sedona</a></li>';
+					/*<<<DEV>>>*/ //cities = '<li><a href="//flagstaff.craigslist.org/">flagstaff / sedona</a></li>';
+
+					var cityCnt = 0;
 
 					$(cities).each(function() {
 						var cityName = $(this).text();
 						var cityUrl = $(this).find("a").attr("href");
 
-						json[0].State.Cities.push({ "Name": cityName, "Url": cityUrl, "Listings": []});
+						json[cnt].State.Cities.push({ "Name": cityName, "Url": cityUrl, "Listings": [] });
 
 						var citySearchUrl = cs.BuildClSearchUrl(cityUrl);
+						//console.log("Listings", json[cnt].State.Cities.length);
 
 						request(citySearchUrl, function(error, response, html) {
 							if(!error) {
@@ -54,13 +57,9 @@ var cs = require("./_cl");
 									var location = $(this).find("small").text().replace("(", "").replace(")", "");
 									var datetime = $(this).find("time").attr("datetime");
 
-									json[0].State.Cities[0].Listings.push({ "Id": id, "Title": title, "Price": price, "Location": location, "DateTime": datetime, "Status": ""});
 									
-									//console.log("id:", id);
-									//console.log("title:", title);
-									//console.log("price:", price);
-									//console.log("datetime:", datetime);
-									//console.log("location:", location);
+
+									//json[cnt].State.Cities[cityCnt].Listings.push({ "Id": id, "Title": title, "Price": price, "Location": location, "DateTime": datetime, "Status": ""});
 								});
 							}
 							else {
@@ -68,46 +67,17 @@ var cs = require("./_cl");
 							}
 						});
 
-						//locationUrls.push(cityUrl);
+						cityCnt += 1;
 					});
+
+					cnt += 1;
 				}
 			});
-			console.log("json", json);
-			return;
 
-			//console.log("locationUrls", locationUrls);
-
-			/*<<<DEV>>>*/ locationUrls = ["http://localhost/cl/results.html"];
-
-			$(locationUrls).each(function(index, value) {
-				var url = cs.BuildClSearchUrl(value);
-				//console.log(url);
-				request(url, function(error, response, html) {
-					if(!error) {
-						var $ = cheerio.load(html);
-
-						//before storing as json
-						//cross-match against ignore.json
-
-						$("h4").prevAll().filter(function() {
-							var id = $(this).attr("data-pid");
-							var title = $(this).find(".hdrlnk").text();
-							var price = $(this).find(".price").first().text().replace("$", "");
-							var location = $(this).find("small").text().replace("(", "").replace(")", "");
-							var datetime = $(this).find("time").attr("datetime");
-							
-							//console.log("id:", id);
-							//console.log("title:", title);
-							//console.log("price:", price);
-							//console.log("datetime:", datetime);
-							//console.log("location:", location);
-						});
-					}
-					else {
-						console.log("Failed to request city search URL");
-					}
-				});
-			});
+			// console.log(json);
+			// fs.writeFile("output.json", JSON.stringify(json, null, 4), function(err) {
+			// 	console.log("json file written");
+			// });
 
 			//res.send('Check your console!');
 		}
