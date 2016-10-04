c43fcaff6c954b12e5a578375a4f23af9bd1efa3
brecklend <brecklend@gmail.com>
2016-10-04 10:49:30 -0700

    Removed development code

--- syncroRojo.js
+++ syncroRojo.js
@@ -6,7 +6,6 @@ var cs = require("./_cl");
 var json = [];
 
 function GetStatesAndCitiesFor(query) {
-	//console.log("GetStatesAndCitiesFor.query", query);
 	var sitesUrl = "http://localhost/cl/sites.html";
 
 	request(sitesUrl, function(error, response, html) {
@@ -37,27 +36,14 @@ function GetStatesAndCitiesFor(query) {
 					jsonCnt += 1;
 				}
 			});
-
-			//console.log(JSON.stringify(json));
 		}
 		else {
 			console.log("request sites failed");
 		}
 	});
-
-	// json.push({"State": {"Name": "Arizona", "Cities": [{"Name": "flagstaff / sedona", "Url": "http://localhost/cl/results.html"},{"Name": "mohave county", "Url": "http://localhost/cl/results.html"}]}});
-	// for(var i = 0; i < json.length; i++) {
-	// 	for(var j = 0; j < json[i].State.Cities.length; j++) {
-	// 		var cityName = json[i].State.Cities[j].Name;
-	// 		var cityUrl = json[i].State.Cities[j].Url;
-
-	// 		GetListingsFor(cityName, cityUrl, AddListingsToCity);
-	// 	}
-	// }
 }
 
 function GetListingsFor(city, url, callback) {
-	//console.log("GetListingsFor.city", city, " url", url);
 	var listings = [];
 
 	request(url, function(error, response, html) {
@@ -71,30 +57,18 @@ function GetListingsFor(city, url, callback) {
 				var location = $(this).find("small").text().replace("(", "").replace(")", "");
 				var datetime = $(this).find("time").attr("datetime");
 
-				//console.log("GetListingsFor", id, title);
 				listings.push({"Id": id, "Title": title, "Price": price, "Location": location, "DateTime": datetime});
 			});
 
-			//console.log("end");
 			callback(city, listings);
 		}
 		else {
 			console.log("request listings fail");
 		}
 	});
-
-	// if(city == "flagstaff / sedona") {
-	// 	listings = [{"Id": "11111", "Title": "One"}, {"Id": "22222", "Title": "Two"}];
-	// }
-	// else {
-	// 	listings = [{"Id": "33333", "Title": "Three"}, {"Id": "44444", "Title": "Four"}];
-	// }
-	
-	// callback(city, listings);
 }
 
 function AddListingsToCity(city, listings) {
-	//console.log("AddListingsToCity.city", city, " listings", listings);
 	for(var i = 0; i < json.length; i++) {
 		for(var j = 0; j < json[i].State.Cities.length; j++) {
 			if(json[i].State.Cities[j].Name == city) {
@@ -104,7 +78,7 @@ function AddListingsToCity(city, listings) {
 	}
 
 	fs.writeFile("syncroRojo.json", JSON.stringify(json, null, 4), function(err) {
-		console.log("wrote file");
+		
 	});
 	//console.log("******************************", JSON.stringify(json));
 }
